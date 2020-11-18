49fd80dcdf42e048c3c27e9e70dbc556a8b06433
kazmanbanj <58645797+kazmanbanj@users.noreply.github.com>
2020-11-17 21:22:18 -0800

    praticing python

--- scripting/sending_mails/email_sender.py
+++ scripting/sending_mails/email_sender.py
@@ -1,17 +1,20 @@
 import smtplib      # this creates a smtp server to send email
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Banjoko Kazeem'
 email['to'] = 'banjoko10@gmail.com'
 email['subject'] = 'Hello there'
 
-email.set_content('I am using Python to send this email, expect more from me later... Bye for now')
+email.set_content(html.substitute({'name': 'Jaho'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('banjoko2020@gmail.com', 'input_password')
+    smtp.login('banjoko2020@gmail.com', '08163282411')
     smtp.send_message(email)
     print('email sent!!!')
 
