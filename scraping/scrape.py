f6bd3e3b1e2b0cbf6593b83e573eeb497a6aa0e5
kazmanbanj <58645797+kazmanbanj@users.noreply.github.com>
2020-11-18 15:46:42 -0800

    practice_On_python

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -1,8 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get('https://news.ycombinator.com/news')
+res = requests.get('https://www.nairaland.com/education')
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.body)
-print(soup.find_all('div'))
-print(soup.find_all('a'))
+# print(soup.body)
+# print(soup.find_all('div'))
+# print(soup.find_all('a'))
+# print(soup.find(id='score_20514755'))
+# print(soup.select('#score_20514755'))
+# print(soup.select('.score'))
+
--- scripting/sms_with_python/sms.py
+++ scripting/sms_with_python/sms.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
  
-account_sid = 'AC7e2f8a7012021aa0cb914138cfa262dc' 
-auth_token = '[cac3b1deb2a6278b2760565c24ed3d44]' 
+account_sid = 'sid_here' 
+auth_token = '[auth_token_here]' 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create( 
